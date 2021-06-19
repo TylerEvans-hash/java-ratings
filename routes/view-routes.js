dcb758f927cd636f7fa1549dc4b9ee790d3cd87c
unknown <tgelety@gmail.com>
2021-06-19 13:42:01 -0700

    moves the renders to a router

--- routes/view-routes.js
+++ routes/view-routes.js
@@ -1,6 +1,37 @@
 const router = require('express').Router();
 
+router.get('/', (req, res) => {
 
+    res.render('home', {
+        dev: [
+            {
+                name: 'Cade Ellsworth',
+                username: 'EEzycade',
+            },
+            {
+                name: 'Tyler Evans',
+                username: 'TylerEvans-hash',
+            },
+            {
+                name: 'Tim Gelety',
+                username: 'Tim-Gelety',
+            },
+            {
+                name: 'Natasha Harrison',
+                username: 'natasharrison',
+            }
+        ]
+    }
+    )
+});
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+    res.render('signup');
+});
 
 
 module.exports = router;
