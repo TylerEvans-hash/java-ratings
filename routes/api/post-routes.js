b8c17c0e600700f155b9f48ef49badaf7f324d97
Red3ffect <red3ffect117@gmail.com>
2021-06-20 14:59:14 -0700

    Adds CRUD routes for User & Post models

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -11,16 +11,77 @@ router.get('/', (req, res) => {
         });
 });
 
-// Get post by ID
-router.get('/:id', (req, res) => {});
+// Get post by post ID
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No Post found with this id' });
+                return;
+            }
+            res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 // Create Post
 router.post('/', (req, res) => {
     Post.create({
         name: req.body.name,
         description: req.body.description,
-        // author: req.body.authorId
-    });
+        authorId: req.body.authorId
+    })
+        .then(dbPostData => res.json(dbPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// Update Post
+router.put('/', (req, res) => {
+    Post.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(400).json({ message: 'No Post found with this id' });
+                return;
+            }
+            res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// Delete Post by ID
+router.delete('/:id', (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).status.json({ message: 'No Post found with this id' });
+                return;
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
