ed4546d044163f076a6a583349c3922e65b9e222
unknown <tgelety@gmail.com>
2021-06-21 00:40:15 -0700

    header page logic

--- routes/view-routes.js
+++ routes/view-routes.js
@@ -1,28 +1,42 @@
 const router = require('express').Router();
+const data = {
+
+    dev: [
+        {
+            name: 'Cade Ellsworth',
+            username: 'EEzycade',
+        },
+        {
+            name: 'Tyler Evans',
+            username: 'TylerEvans-hash',
+        },
+        {
+            name: 'Tim Gelety',
+            username: 'Tim-Gelety',
+        },
+        {
+            name: 'Natasha Harrison',
+            username: 'natasharrison',
+        }
+    ],
+
+    loggedOut:
+    {
+        buttonA: 'login',
+        buttonB: 'signup',
+    },
+    loggedIn:
+    {
+        buttonA: 'logout',
+        buttonB: 'createPost',
+    },
+
+    user: true,
+}
 
 router.get('/', (req, res) => {
 
-    res.render('home', {
-        dev: [
-            {
-                name: 'Cade Ellsworth',
-                username: 'EEzycade',
-            },
-            {
-                name: 'Tyler Evans',
-                username: 'TylerEvans-hash',
-            },
-            {
-                name: 'Tim Gelety',
-                username: 'Tim-Gelety',
-            },
-            {
-                name: 'Natasha Harrison',
-                username: 'natasharrison',
-            }
-        ]
-    }
-    )
+    res.render('home', data)
 });
 
 router.get('/login', (req, res) => {
