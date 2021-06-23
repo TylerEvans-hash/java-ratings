f8999df7da4c4b15da46536acefde25d78b3d127
Red3ffect <red3ffect117@gmail.com>
2021-06-22 21:56:46 -0700

    Adds something

--- config/connection.js
+++ config/connection.js
@@ -4,10 +4,12 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create connection to our database, pass in your mySQL info
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: process.env.CLEARDB_DATABASE_URL,
-    dialect: 'mysql',
-    port: 3306
-});
+const sequelize = process.env.CLEARDB_DATABASE_URL ?
+    new Sequelize(process.env.CLEARDB_DATABASE_URL) :
+    new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
 
 module.exports = sequelize;
