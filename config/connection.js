e94dd118a6b1f694d7af4ac5cf23860f07ab7535
Red3ffect <red3ffect117@gmail.com>
2021-06-22 21:31:11 -0700

    Adds heroku env

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 // create connection to our database, pass in your mySQL info
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
+    host: process.env.CLEARDB_DATABASE_URL,
     dialect: 'mysql',
     port: 3306
 });
