d43421617b2e6c8b0a22e8ba5f16420f8e80cafd
unknown <tgelety@gmail.com>
2021-06-23 01:46:07 -0700

    signup is working adds login

--- public/js/login.js
+++ public/js/login.js
@@ -1,34 +1,25 @@
-async function signupFormHandler(event) {
+async function loginFormHandler(event) {
     event.preventDefault();
-
-    console.log("hello world!");
-
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-
-    console.log(username, email, password);
-
-    if (username && email && password) {
-        const response = await fetch('/api/users', {
-            method: 'post',
-            body: JSON.stringify({
-                username,
-                email,
-                password
-            }),
-            headers: { 'Content-Type': 'application/json' }
-        });
-
-        if (response.ok) {
-            document.location.replace('/');
-        } else {
-            alert(response.statusText);
-        }
+  
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+  
+    if (email && password) {
+      const response = await fetch('/api/users/login', {
+        method: 'post',
+        body: JSON.stringify({
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      if (response.ok) {
+        console.log('OK')
+      } else {
+        alert(response.statusText);
+      }
     }
-}
+  }
 
-document.querySelector('.signup-form-btn').addEventListener('click', signupFormHandler);
-
-// const url = window.location.origin;
-// console.log(url);
+  document.querySelector('.login-form-btn').addEventListener('click', loginFormHandler);
