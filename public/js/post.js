405dc53e597fce51eab0469d66eb69c0cba47c78
unknown <tgelety@gmail.com>
2021-06-23 14:29:02 -0700

    postjs tied to postroutes

--- public/js/post.js
+++ public/js/post.js
@@ -2,31 +2,31 @@
 async function newFormHandler(event) {
     event.preventDefault();
 
-    const title = document.querySelector('input[name="post-title"]').value;
-    const description = document.querySelector('input[name="description"]').value;
-    const photo = document.querySelector('input[name="image"]').value;
+    const title = document.querySelector('#title').value;
+    const description = document.querySelector('#review').value;
+    const photo = document.querySelector('#image').value;
     // const username - how would we grab this? 
     // getting a session data if they are logged in
 
     // what is the appropriate api route we need to use?
-    const response = await fetch(`/api/posts`, {
-        method: 'post',
-        body: JSON.stringify({
-            title,
-            description,
-            photo,
-            username
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-            // not sure what this means
-        }
-    });
+    if (title && description && photo) {
+        const response = await fetch(`/api/posts/`, {
+            method: 'post',
+            body: JSON.stringify({
+                title,
+                description,
+                photo
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
 
-    if (response.ok) {
-        document.location.replace('/');
-    } else {
-        alert(response.statusText);
+        if (response.ok) {
+            console.log(response)
+            // sends you back to the home page
+            // document.location.replace('/');
+        } else {
+            alert(response.statusText);
+        }
     }
 }
 
